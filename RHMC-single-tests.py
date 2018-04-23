596a13c4c078e770779c896cf2c0644d0b74769e
Jae Hyeon Lee <jaekor91@gmail.com>
2018-04-23 10:49:22 -0400

    Added an option to choose seperation

--- RHMC-single-tests.py
+++ RHMC-single-tests.py
@@ -14,6 +14,9 @@ mag_model_list = [15., 18., 20., 21.]
 # Case counter: mT and mM pair counts as a case.
 counter_case = 0
 
+# Separation
+sep_list = [0, 1.]
+
 # Save directory
 save_dir = "./RHMC-single-traj/"
 
@@ -33,18 +36,21 @@ for mT in mag_true_list:
 		counter_case += 1
 		if counter_case < 2:
 			print "/----- Case %d: mT = %2d, mM = %d" % (counter_case, mT, mM)
-			q_model = np.array([[mM, gym.num_rows /2. + 2., gym.num_cols /2.]])
-			for i in range(len(Nsteps_list)):
-				# Number of steps and time step size
-				Nsteps = Nsteps_list[i]
-				dt = dt_list[i]
-				print "Nsteps: %d" % Nsteps
+			for sep in sep_list:
+				print "/--- sep: %.1f" % sep
+				q_model = np.array([[mM, gym.num_rows /2. + sep, gym.num_cols /2.]])
+				for i in range(len(Nsteps_list)):
+					# Number of steps and time step size
+					Nsteps = Nsteps_list[i]
+					dt = dt_list[i]
+					print "Nsteps: %d" % Nsteps
 
-				# Run the simulation.
-				gym.Nsteps = Nsteps
-				gym.dt = dt
-				gym.run_single_RHMC(q_model_0=np.copy(q_model), f_pos=True, solver="implicit", delta=1e-6)
-				diag_str1 = save_dir + "%d-mT%d-mM%d-H-Nsteps%d.png" % (counter_case, mT, mM, Nsteps)
-				diag_str2 = save_dir + "%d-mT%d-mM%d-HVT-Nsteps%d.png" % (counter_case, mT, mM, Nsteps)
-				gym.diagnostics_first(q_true, plot_flux=False, save=diag_str1, show=False)
-				gym.diagnostics_first(q_true, plot_flux=False, plot_T=True, plot_V=True, save=diag_str2, show=False)
+					# Run the simulation.
+					gym.Nsteps = Nsteps
+					gym.dt = dt
+
+					gym.run_single_RHMC(q_model_0=np.copy(q_model), f_pos=True, solver="implicit", delta=1e-6)
+					diag_str1 = save_dir + "%d-mT%d-mM%d-sep%d-H-Nsteps%d.png" % (counter_case, mT, mM, sep, Nsteps)
+					diag_str2 = save_dir + "%d-mT%d-mM%d-sep%d-HVT-Nsteps%d.png" % (counter_case, mT, mM, sep, Nsteps)
+					gym.diagnostics_first(q_true, plot_flux=False, save=diag_str1, show=False)
+					gym.diagnostics_first(q_true, plot_flux=False, plot_T=True, plot_V=True, save=diag_str2, show=False)
