0595aaa08f5aa53bda1b3ca38f430162faad1c9a
Jae Hyeon Lee <jaekor91@gmail.com>
2018-04-23 14:20:46 -0400

    Recycle the same p when simulating with different step sizes

--- RHMC-single-tests.py
+++ RHMC-single-tests.py
@@ -8,8 +8,8 @@ Nsteps_list = [100, 1000, 10000, 100000]
 dt_list = [1e-1, 1e-2, 1e-3, 1e-4]
 
 # Cross checked
-mag_true_list = [15., 18., 20., 21.]
-mag_model_list = [15., 18., 20., 21.]
+mag_true_list = [15, 19., 20., 21.]
+mag_model_list = [15., 19., 20., 21.]
 
 # Case counter: mT and mM pair counts as a case.
 counter_case = 0
@@ -39,6 +39,8 @@ for mT in mag_true_list:
 		for sep in sep_list:
 			print "/--- sep: %.1f" % sep
 			q_model = np.array([[mM, gym.num_rows /2. + sep, gym.num_cols /2.]])
+
+			p_initial = None
 			for i in range(len(Nsteps_list)):
 				# Number of steps and time step size
 				Nsteps = Nsteps_list[i]
@@ -49,8 +51,12 @@ for mT in mag_true_list:
 				gym.Nsteps = Nsteps
 				gym.dt = dt
 
-				gym.run_single_RHMC(q_model_0=np.copy(q_model), f_pos=True, solver="implicit", delta=1e-6)
+				# Note: Recycle the p_initial if available
+				gym.run_single_RHMC(q_model_0=np.copy(q_model), f_pos=True, solver="implicit", delta=1e-6, p_initial = p_initial)
 				diag_str1 = save_dir + "%d-mT%d-mM%d-sep%d-H-Nsteps%d.png" % (counter_case, mT, mM, sep, Nsteps)
 				diag_str2 = save_dir + "%d-mT%d-mM%d-sep%d-HVT-Nsteps%d.png" % (counter_case, mT, mM, sep, Nsteps)
 				gym.diagnostics_first(q_true, plot_flux=False, save=diag_str1, show=False)
 				gym.diagnostics_first(q_true, plot_flux=False, plot_T=True, plot_V=True, save=diag_str2, show=False)
+
+				if p_initial is None:
+					p_initial = gym.p_chain[0, :]
