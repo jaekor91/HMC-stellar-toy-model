b3a4819504654ae990505486cd2ba391c5edab89
Jae Hyeon Lee <jaekor91@gmail.com>
2018-04-27 01:01:28 -0400

    Plot changes to the big sim 2

--- RHMC-big-sim2.py
+++ RHMC-big-sim2.py
@@ -4,21 +4,22 @@ from sampler_RHMC import *
 # Number of steps
 Nsteps = 30
 Niter = 1000
-dt = 1e-2
+dt = 5e-3
 prior = True
 
+gff2_list = scheduler(1/25., 2., 200)
 gym = multi_gym(dt=0., Nsteps=0, g_xx=0.005, g_ff=25., g_ff2=2.)
 
 # --- Multiple stars
 np.random.seed(77)
 gym.num_rows = gym.num_cols = 32
 Nobjs = 30
-Nobjs_model = 35
+Nobjs_model = 50
 q_true = np.zeros((Nobjs, 3))
 q_model = np.zeros((Nobjs_model, 3))
 
 # ---- Truth samples
-alpha = 1.7
+alpha = 1.5
 if prior:
     gym.use_prior = True
     gym.alpha = alpha
@@ -50,24 +51,27 @@ q_model[:, 0] = mag
 q_model[:, 1] = x 
 q_model[:, 2] = y 
 
-# # Generate mock data
+# Generate mock data
 gym.gen_mock_data(q_true)
 # gym.display_image()
+	
+# Generate noise profile
+gym.gen_noise_profile(q_true, N_trial=1000)
 
 # print "--------------- RHMC"
 gym.run_RHMC(q_model, f_pos=True, delta=1e-6, Niter = Niter, Nsteps=Nsteps, \
-             dt = dt, save_traj=False, verbose=True, q_true = q_true)
+             dt = dt, save_traj=False, verbose=True, q_true = q_true, schedule_g_ff2=gff2_list)
 
 
 
-# save_dir = "./RHMC-big-sim3/"
-# counter = 0
-# j = 0
-# for i in xrange(0, Niter+1):
-#     # for j in xrange(Nsteps+1):
-#     title_str = "Niter%05d-Step%03d" % (i, j)
-#     fname = save_dir + "slide-%07d" % counter
-#     gym.diagnostics_all(q_true, show=False, idx_iter = i, idx_step=j, save=fname,\
-#                m=-15, b =10, s0=23, y_min=5., title_str=title_str)
-#     counter+=1 
-#         
+save_dir = "./RHMC-big-sim2/"
+counter = 0
+j = 0
+for i in xrange(0, Niter+1):
+    # for j in xrange(Nsteps+1):
+    title_str = "Niter%05d-Step%03d" % (i, j)
+    fname = save_dir + "slide-%07d" % counter
+    gym.diagnostics_all(q_true, show=False, idx_iter = i, idx_step=j, save=fname,\
+               m=-15, b =10, s0=23, y_min=5., title_str=title_str)
+    counter+=1 
+        
