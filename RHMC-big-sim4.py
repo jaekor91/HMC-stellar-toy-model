7ce53d5114e8cac297201a06e65726ac0178a063
Jae Hyeon Lee <jaekor91@gmail.com>
2018-05-15 20:15:35 -0400

    The latest and largest sim that worked.

--- RHMC-big-sim4.py
+++ RHMC-big-sim4.py
@@ -2,12 +2,12 @@ from utils import *
 from sampler_RHMC import *
 
 # Number of steps
-Nsteps = 20
-Niter = 1000
+Nsteps = 10
+Niter = 10000
 prior = True
 use_Vc = False
 RJRHMC = True # Use Transdimensional moves
-P_move = [0.5, 0.5, 0.]
+P_move = [0.6, 0.2, 0.2]
 dt = 5e-2
 
 # gff2_list = scheduler(1/10., 4., 500)
@@ -17,8 +17,8 @@ gym = multi_gym(dt=0., Nsteps=0, g_xx=0.05, g_ff=4., g_ff2=4.)
 # --- Multiple stars
 np.random.seed(77)
 gym.num_rows = gym.num_cols = 32
-Nobjs = 100
-Nobjs_model = 100
+Nobjs = int(32**2 * 0.05)
+Nobjs_model = 5
 q_true = np.zeros((Nobjs, 3))
 q_model = np.zeros((Nobjs_model, 3))
 
@@ -27,7 +27,7 @@ q_model = np.zeros((Nobjs_model, 3))
 
 # ---- Truth samples
 alpha = 2.
-mag_max = 20.5
+mag_max = 20.
 mag_min = 15.
 fmin = gym.mag2flux_converter(mag_max)
 fmax = gym.mag2flux_converter(mag_min)
@@ -39,6 +39,9 @@ for i in xrange(Nobjs):
 if RJRHMC:
     gym.fmin = fmin
     gym.fmax = fmax
+    gym.K_split = 1.
+    gym.beta_a = 4. 
+    gym.beta_b = 4.  
 if prior:
     gym.use_prior = True
     gym.alpha = alpha
@@ -75,14 +78,15 @@ gym.run_RHMC(q_model, f_pos=True, delta=1e-6, Niter = Niter, Nsteps=Nsteps, \
 # schedule_g_ff2=gff2_list, \
 
 
-save_dir = "./RHMC-big-sim4/"
-counter = 0
-j = 0
-for i in xrange(0, Niter+1):
-    # for j in xrange(Nsteps+1):
-    title_str = "Niter%05d-Step%03d" % (i, j)
-    fname = save_dir + "slide-%07d" % counter
-    gym.diagnostics_all(q_true, show=False, idx_iter = i, idx_step=j, save=fname,\
-               m=-15, b =10, s0=23, y_min=5., title_str=title_str)
-    counter+=1 
-        
+# save_dir = "./RHMC-big-sim4/"
+# counter = 0
+# j = 0
+# for i in xrange(0, Niter+1):
+#     if (i % 50) == 0:
+#         # for j in xrange(Nsteps+1):
+#         title_str = "Niter%05d-Step%03d" % (i, j)
+#         fname = save_dir + "slide-%07d" % counter
+#         gym.diagnostics_all(q_true, show=False, idx_iter = i, idx_step=j, save=fname,\
+#                    m=-15, b =10, s0=23, y_min=5., title_str=title_str)
+#         counter+=1 
+#         
